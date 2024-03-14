93a911be3f26e15b6096b16c35ad13a244ab9af6
Sothesom <151612168+sotheso@users.noreply.github.com>
2024-03-14 00:49:23 -0700

    Initial Commit

---
+++ DakkeApp/DakkeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DakkeAppApp.swift
+//  DakkeApp
+//
+//  Created by Sothesom on 24/12/1402.
+//
+
+import SwiftUI
+
+@main
+struct DakkeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
